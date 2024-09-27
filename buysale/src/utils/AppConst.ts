9726dc5e44417e4b98b942b4b5e9ce763a4d5f04
akashkumar62 <xiome555@gmail.com>
2024-09-27 15:45:28 +0530

    microservices

---
+++ buysale/src/utils/AppConst.ts
@@ -0,0 +1,4 @@
+export const BASE_URL =
+  "https://i9zuzpsxp2.execute-api.eu-central-1.amazonaws.com"; //"http://localhost:4000"; // Your local customer service URL
+export const TRANSACTION_URL =
+  "https://4favyg1qn0.execute-api.eu-central-1.amazonaws.com/prod";
